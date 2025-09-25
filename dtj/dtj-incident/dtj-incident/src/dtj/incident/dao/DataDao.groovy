ab60bd6f3b5ffc99e83b7cf40c390ab05257cffe
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-09-25 16:01:48 +0500

    new project [Incident]

---
+++ dtj/dtj-incident/dtj-incident/src/dtj/incident/dao/DataDao.groovy
@@ -0,0 +1,91 @@
+package dtj.incident.dao
+
+import groovy.transform.CompileStatic
+import jandcode.commons.UtCnv
+import jandcode.commons.datetime.XDate
+import jandcode.commons.datetime.XDateTime
+import jandcode.commons.datetime.XDateTimeFormatter
+import jandcode.commons.error.XError
+import jandcode.commons.variant.VariantMap
+import jandcode.core.auth.AuthService
+import jandcode.core.auth.AuthUser
+import jandcode.core.dao.DaoMethod
+import jandcode.core.dbm.mdb.BaseMdbUtils
+import jandcode.core.std.CfgService
+import jandcode.core.store.Store
+import jandcode.core.store.StoreIndex
+import jandcode.core.store.StoreRecord
+import tofi.api.adm.ApiAdm
+import tofi.api.dta.ApiInspectionData
+import tofi.api.dta.ApiNSIData
+import tofi.api.dta.ApiObjectData
+import tofi.api.dta.ApiOrgStructureData
+import tofi.api.dta.ApiPersonnalData
+import tofi.api.dta.ApiPlanData
+import tofi.api.dta.ApiUserData
+import tofi.api.dta.model.utils.EntityMdbUtils
+import tofi.api.dta.model.utils.UtPeriod
+import tofi.api.mdl.ApiMeta
+import tofi.api.mdl.model.consts.FD_AttribValType_consts
+import tofi.api.mdl.model.consts.FD_InputType_consts
+import tofi.api.mdl.model.consts.FD_PeriodType_consts
+import tofi.api.mdl.model.consts.FD_PropType_consts
+import tofi.apinator.ApinatorApi
+import tofi.apinator.ApinatorService
+
+
+@CompileStatic
+class DataDao extends BaseMdbUtils {
+
+    ApinatorApi apiAdm() {
+        return app.bean(ApinatorService).getApi("adm")
+    }
+    ApinatorApi apiMeta() {
+        return app.bean(ApinatorService).getApi("meta")
+    }
+    ApinatorApi apiUserData() {
+        return app.bean(ApinatorService).getApi("userdata")
+    }
+    ApinatorApi apiNSIData() {
+        return app.bean(ApinatorService).getApi("nsidata")
+    }
+    ApinatorApi apiObjectData() {
+        return app.bean(ApinatorService).getApi("objectdata")
+    }
+    ApinatorApi apiPlanData() {
+        return app.bean(ApinatorService).getApi("plandata")
+    }
+    ApinatorApi apiPersonnalData() {
+        return app.bean(ApinatorService).getApi("personnaldata")
+    }
+    ApinatorApi apiOrgStructureData() {
+        return app.bean(ApinatorService).getApi("orgstructuredata")
+    }
+    ApinatorApi apiInspectionData() {
+        return app.bean(ApinatorService).getApi("incpectiondata")
+    }
+    /* =================================================================== */
+
+
+
+
+    @DaoMethod
+    Map<String, Object> getCurUserInfo() {
+        AuthService authSvc = mdb.getApp().bean(AuthService.class)
+        AuthUser au = authSvc.getCurrentUser()
+        if (au == null) {
+            throw new XError("NotLogined")
+        }
+        return au.getAttrs()
+    }
+
+
+    private long getUser() throws Exception {
+        AuthService authSvc = mdb.getApp().bean(AuthService.class)
+        long au = authSvc.getCurrentUser().getAttrs().getLong("id")
+        if (au == 0)
+            au = 1//throw new XError("notLogined")
+        return au
+    }
+
+}
---
+++ dtj/dtj-incident/dtj-incident/test/dtj/incident/test_obj/Obj_Test.groovy
@@ -0,0 +1,17 @@
+package dtj.incident.test_obj
+
+import dtj.incident.dao.DataDao
+import jandcode.core.apx.test.Apx_Test
+import jandcode.core.store.Store
+import jandcode.core.store.StoreRecord
+import org.junit.jupiter.api.Test
+
+class Obj_Test extends Apx_Test {
+
+    @Test
+    void loadPersonnal() {
+        DataDao dao = mdb.createDao(DataDao.class)
+    }
+
+
+}
