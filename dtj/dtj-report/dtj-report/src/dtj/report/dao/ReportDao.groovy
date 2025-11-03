ec2bce5698f7b3390db30ad39efffcc59de29bf9
Tolybay.Kuanov <tolybay.zh.k@gmail.com>
2025-11-03 11:15:16 +0500

    DTJ Report Service

---
+++ dtj/dtj-report/dtj-report/src/dtj/report/dao/ReportDao.groovy
@@ -0,0 +1,102 @@
+package dtj.report.dao
+
+import groovy.transform.CompileStatic
+import jandcode.commons.error.XError
+import jandcode.core.auth.AuthService
+import jandcode.core.dbm.mdb.BaseMdbUtils
+import jandcode.core.store.Store
+import tofi.api.dta.ApiClientData
+import tofi.api.dta.ApiInspectionData
+import tofi.api.dta.ApiNSIData
+import tofi.api.dta.ApiObjectData
+import tofi.api.dta.ApiOrgStructureData
+import tofi.api.dta.ApiPersonnalData
+import tofi.api.dta.ApiPlanData
+import tofi.api.dta.ApiRepairData
+import tofi.api.dta.ApiResourceData
+import tofi.api.dta.ApiUserData
+import tofi.api.mdl.ApiMeta
+import tofi.apinator.ApinatorApi
+import tofi.apinator.ApinatorService
+
+@CompileStatic
+class ReportDao extends BaseMdbUtils {
+
+    ApinatorApi apiMeta() {
+        return app.bean(ApinatorService).getApi("meta")
+    }
+    ApinatorApi apiUserData() {
+        return app.bean(ApinatorService).getApi("userdata")
+    }
+    ApinatorApi apiNSIData() {
+        return app.bean(ApinatorService).getApi("nsidata")
+    }
+    ApinatorApi apiPersonnalData() {
+        return app.bean(ApinatorService).getApi("personnaldata")
+    }
+    ApinatorApi apiOrgStructureData() {
+        return app.bean(ApinatorService).getApi("orgstructuredata")
+    }
+    ApinatorApi apiObjectData() {
+        return app.bean(ApinatorService).getApi("objectdata")
+    }
+    ApinatorApi apiPlanData() {
+        return app.bean(ApinatorService).getApi("plandata")
+    }
+    ApinatorApi apiInspectionData() {
+        return app.bean(ApinatorService).getApi("inspectiondata")
+    }
+    ApinatorApi apiClientData() {
+        return app.bean(ApinatorService).getApi("clientdata")
+    }
+    ApinatorApi apiRepairData() {
+        return app.bean(ApinatorService).getApi("repairdata")
+    }
+    ApinatorApi apiResourceData() {
+        return app.bean(ApinatorService).getApi("resourcedata")
+    }
+
+
+
+    //-------------------------
+
+    private Store loadSqlMeta(String sql, String domain) {
+        return apiMeta().get(ApiMeta).loadSql(sql, domain)
+    }
+
+    private Store loadSqlService(String sql, String domain, String model) {
+        if (model.equalsIgnoreCase("userdata"))
+            return apiUserData().get(ApiUserData).loadSql(sql, domain)
+        else if (model.equalsIgnoreCase("nsidata"))
+            return apiNSIData().get(ApiNSIData).loadSql(sql, domain)
+        else if (model.equalsIgnoreCase("personnaldata"))
+            return apiPersonnalData().get(ApiPersonnalData).loadSql(sql, domain)
+        else if (model.equalsIgnoreCase("orgstructuredata"))
+            return apiOrgStructureData().get(ApiOrgStructureData).loadSql(sql, domain)
+        else if (model.equalsIgnoreCase("objectdata"))
+            return apiObjectData().get(ApiObjectData).loadSql(sql, domain)
+        else if (model.equalsIgnoreCase("plandata"))
+            return apiPlanData().get(ApiPlanData).loadSql(sql, domain)
+        else if (model.equalsIgnoreCase("inspectiondata"))
+            return apiInspectionData().get(ApiInspectionData).loadSql(sql, domain)
+        else if (model.equalsIgnoreCase("clientdata"))
+            return apiClientData().get(ApiClientData).loadSql(sql, domain)
+        else if (model.equalsIgnoreCase("repairdata"))
+            return apiRepairData().get(ApiRepairData).loadSql(sql, domain)
+        else if (model.equalsIgnoreCase("resourcedata"))
+            return apiResourceData().get(ApiResourceData).loadSql(sql, domain)
+        else
+            throw new XError("Unknown model [${model}]")
+    }
+
+
+    private long getUser() throws Exception {
+        AuthService authSvc = mdb.getApp().bean(AuthService.class)
+        long au = authSvc.getCurrentUser().getAttrs().getLong("id")
+        if (au == 0)
+            au = 1//throw new XError("notLogined")
+        return au
+    }
+
+
+}
---
+++ dtj/dtj-report/dtj-report/test/dtj/report/Report_Test.groovy
@@ -0,0 +1,16 @@
+package dtj.report
+
+import jandcode.core.apx.test.Apx_Test
+import org.junit.jupiter.api.Test
+
+class Report_Test extends Apx_Test {
+
+    @Test
+    void report1_test () {
+
+    }
+
+
+
+
+}
