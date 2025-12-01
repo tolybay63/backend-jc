b321d2daabe87bc651455cb7c05924e45d13b82f
Kazybek <seidakhmetov.kazybek@gmail.com>
2025-12-01 11:26:31 +0500

    upd

--- dtj/dtj-repair/dtj-repair/src/dtj/repair/dao/DataDao.groovy
+++ dtj/dtj-repair/dtj-repair/src/dtj/repair/dao/DataDao.groovy
@@ -338,7 +338,7 @@ class DataDao extends BaseMdbUtils {
                     select o.id, o.cls, '[№' || coalesce(v1.strVal, '') ||'] ' || v.name as name
                     from Obj o
                         left join ObjVer v on o.id=v.ownerver and v.lastver=1
-                        left join DataProp d1 on d1.objorrelobj=o.id and d1.prop=:Prop_Number
+                        left join DataProp d1 on d1.objorrelobj=o.id and d1.prop=${map.get("Prop_Number")}
                         left join DataPropVal v1 on d1.id=v1.dataprop
                     where o.id in (0${idsTool.join(",")})
                 """, "", "resourcedata")
