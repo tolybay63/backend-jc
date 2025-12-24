9bf4cc2594a825a4d29f93be0e52e262c67152d4
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-12-24 22:59:14 +0500

    upd

--- dtj/dtj-inspection/dtj-inspection/test/dtj/inspection/test_obj/TestXml.groovy
+++ dtj/dtj-inspection/dtj-inspection/test/dtj/inspection/test_obj/TestXml.groovy
@@ -9,7 +9,11 @@ class TestXml extends Apx_Test {
     @Test
     void test() {
         //File inputFile = new File("C:\\jc-2\\_info\\xml\\G057_22042025_113706_64_1 1.xml")
-        File inputFile = new File("C:\\jc-2\\_info\\xml\\B057_22042025_113706_1 1.xml")
+        //File inputFile = new File("C:\\jc-2\\_info\\xml\\B057_22042025_113706_1 1.xml")
+
+        //File inputFile = new File("D:\\backup\\xml\\G057_22042025_113706_64_1 1.xml")
+        File inputFile = new File("D:\\backup\\xml\\B057_22042025_113706_1 1.xml")
+
         ImportDao dao = mdb.createDao(ImportDao.class)
         dao.analyze(inputFile)
     }
