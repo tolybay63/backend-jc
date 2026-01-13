d13170d3f0e64775196394bbfc5b2bb029d45176
Tolybay.Kuanov <tolybay.zh@gmail.com>
2026-01-13 09:47:11 +0500

    upd && add

---
+++ tofi-mdl/tofi-mdl/src/tofi/mdl/model/dao/stock/StockDao.java
@@ -0,0 +1,43 @@
+package tofi.mdl.model.dao.stock;
+
+import jandcode.core.dbm.dao.BaseModelDao;
+import jandcode.core.store.Store;
+import jandcode.core.store.StoreRecord;
+import tofi.mdl.model.dao.database.DataBaseMdbUtils;
+
+import java.util.Map;
+
+public class StockDao extends BaseModelDao {
+
+
+    public Store loadStocks(long stockGr) throws Exception {
+        StockMdbUtils utils = new StockMdbUtils(getMdb(), "SourceStock");
+        return utils.loadStocks(stockGr);
+    }
+
+    public StoreRecord newRec(long stockGr) throws Exception {
+        StockMdbUtils utils = new StockMdbUtils(getMdb(), "SourceStock");
+        return utils.newRec(stockGr);
+    }
+
+    public Store insert(Map<String, Object> rec) throws Exception {
+        StockMdbUtils utils = new StockMdbUtils(getMdb(), "SourceStock");
+        return utils.insert(rec);
+    }
+
+    public Store update(Map<String, Object> rec) throws Exception {
+        StockMdbUtils utils = new StockMdbUtils(getMdb(), "SourceStock");
+        return utils.update(rec);
+    }
+
+    public void delete(Map<String, Object> rec) throws Exception {
+        StockMdbUtils utils = new StockMdbUtils(getMdb(), "SourceStock");
+        utils.delete(rec);
+    }
+
+    public Store loadStockForSelect() throws Exception {
+        StockMdbUtils utils = new StockMdbUtils(getMdb(), "SourceStock");
+        return utils.loadStockForSelect();
+    }
+
+}
---
+++ tofi-mdl/tofi-mdl/src/tofi/mdl/model/dao/stock/StockMdbUtils.java
@@ -0,0 +1,74 @@
+package tofi.mdl.model.dao.stock;
+
+import jandcode.commons.UtCnv;
+import jandcode.commons.error.XError;
+import jandcode.core.dbm.mdb.Mdb;
+import jandcode.core.store.Store;
+import jandcode.core.store.StoreRecord;
+import tofi.mdl.consts.FD_AccessLevel_consts;
+import tofi.mdl.consts.FD_DataBaseType_consts;
+import tofi.mdl.consts.FD_SourceStockType_consts;
+import tofi.mdl.model.utils.EntityMdbUtils;
+
+import java.util.Map;
+
+public class StockMdbUtils extends EntityMdbUtils {
+    Mdb mdb;
+    String tableName;
+
+    public StockMdbUtils(Mdb mdb, String tableName) {
+        super(mdb, tableName);
+        this.mdb = mdb;
+        this.tableName = tableName;
+    }
+
+
+
+    public Store loadStocks(long stockGr) throws Exception {
+        Store st = mdb.createStore("SourceStock");
+        mdb.loadQuery(st, """
+            select * from SourceStock where parent=:p
+        """, Map.of("p", stockGr));
+        return st;
+    }
+
+    public StoreRecord newRec(long stockGr) {
+        Store st = mdb.createStore("SourceStock");
+        StoreRecord r = st.add();
+        r.set("parent", stockGr);
+        r.set("accessLevel", FD_AccessLevel_consts.common);
+        r.set("sourceStockType", FD_SourceStockType_consts.file);
+        return  r;
+    }
+
+    public Store insert(Map<String, Object> rec) throws Exception {
+        long id = insertEntity(rec);
+
+        Store st = mdb.createStore("SourceStock");
+        mdb.loadQuery(st, "select * from SourceStock where id=:id", Map.of("id", id));
+        return st;
+    }
+
+    public Store update(Map<String, Object> rec) throws Exception {
+        long id = UtCnv.toLong(rec.get("id"));
+        if (id == 0) {
+            throw new XError("Поле id должно иметь не нулевое значение");
+        }
+        updateEntity(rec);
+
+        Store st = mdb.createStore("SourceStock");
+        mdb.loadQuery(st, "select * from SourceStock where id=:id", Map.of("id", id));
+        return st;
+    }
+
+    public void delete(Map<String, Object> rec) throws Exception {
+        deleteEntity(rec);
+    }
+
+    public Store loadStockForSelect() throws Exception {
+        return mdb.loadQuery("""
+            select id, name from SourceStock where 0=0
+        """);
+    }
+
+}
