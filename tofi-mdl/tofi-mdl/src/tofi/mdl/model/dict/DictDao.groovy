efccd02b01abad5e133f04aa205aaedae9cbf01d
Tolybay.Kuanov <tolybay.zh@gmail.com>
2026-02-03 19:03:05 +0500

    upd

--- tofi-mdl/tofi-mdl/src/tofi/mdl/model/dict/DictDao.groovy
+++ tofi-mdl/tofi-mdl/src/tofi/mdl/model/dict/DictDao.groovy
@@ -1,45 +1,44 @@
 package tofi.mdl.model.dict
 
-import jandcode.commons.UtCnv
+
 import jandcode.commons.error.XError
 import jandcode.core.auth.AuthService
 import jandcode.core.auth.AuthUser
-import jandcode.core.dbm.ModelService
 import jandcode.core.dbm.dao.BaseModelDao
-import jandcode.core.dbm.dict.DictData
-import jandcode.core.dbm.dict.DictService
-import jandcode.core.dbm.domain.Domain
 import jandcode.core.store.Store
 
 class DictDao extends BaseModelDao {
-
     Store load(Map<String, Object> params) throws Exception {
-        ModelService modelSvc = getApp().bean(ModelService.class);
-        DictService dictSvc =  modelSvc.getModel("default").bean(DictService.class);
-        DictData dd = dictSvc.loadDictData(UtCnv.toString(params.get("dict")));
-        //
-        Domain dom = mdb.createDomain(dd.data)
-        Store st = mdb.createStore(dom)
-        if (!dd.name.equalsIgnoreCase("FD_AccessLevel")) {
-            dd.data.forEach(r -> {
-                if (r.getBoolean("vis"))
-                    st.add(r)
-            })
+        Store st = mdb.createStore("FD_DictsLang")
+        String dict = params.get("dict").toString()
+        if (!dict.equalsIgnoreCase("FD_AccessLevel")) {
+            mdb.loadQuery(st, """
+                select d.id, l.text
+                from ${dict} d
+                inner join FD_DictsLang l on d.id=l.idDict and l.nameDict=lower(:dict) and l.lang=:lang
+                where d.vis=1
+                order by d.ord
+            """, params)
+
         } else {
             AuthService authSvc = mdb.getApp().bean(AuthService.class);
             AuthUser au = authSvc.getCurrentUser();
             long al = au.getAttrs().getLong("accesslevel");
             if (al==0)
                 throw new XError("notLogined")
-            st.clear()
-            dd.data.forEach(r -> {
-                if (r.getBoolean("vis") && r.getLong("id") <= al)
-                    st.add(r)
-            })
+            params.put("al", al)
+            mdb.loadQuery(st, """
+                select d.id, l.text
+                from FD_AccessLevel d
+                inner join FD_DictsLang l on d.id=l.idDict and l.nameDict=lower(:dict) and l.lang=:lang
+                where d.vis=1 and d.id<=:al 
+                order by d.ord
+            """, params)
+
         }
         //
+        //mdb.outTable(st)
         return st;
     }
-
 
 }
